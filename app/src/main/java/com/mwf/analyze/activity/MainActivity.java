1b66f3b18e93fdcd564878a15f255fb1201fda3e
mocn2619 <mocn26169@qq.com>
2017-03-06 18:03:26 +0800

    识别文件

--- app/src/main/java/com/mwf/analyze/activity/MainActivity.java
+++ app/src/main/java/com/mwf/analyze/activity/MainActivity.java
@@ -2,18 +2,25 @@ package com.mwf.analyze.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.github.angads25.filepicker.controller.DialogSelectionListener;
+import com.github.angads25.filepicker.model.DialogConfigs;
+import com.github.angads25.filepicker.model.DialogProperties;
+import com.github.angads25.filepicker.view.FilePickerDialog;
 import com.mwf.analyze.Constant;
 import com.mwf.analyze.R;
 import com.mwf.analyze.bean.CloudResultPlainParse;
 import com.mwf.analyze.bean.FamousInfoReq;
 import com.mwf.analyze.model.FamousInfoModel;
+import com.mwf.analyze.utils.FileUtils;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -51,22 +58,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.button_search) {
             parseEditext();
         } else if (view.getId() == R.id.button_file) {
-            parseEditext();
+            parseFile();
         }
     }
 
-    private FamousInfoReq initParams() {
+    private FamousInfoReq initParams(String text) {
         FamousInfoReq mFamousInfoReq = null;
         mFamousInfoReq = new FamousInfoReq();
         mFamousInfoReq.api_key = Constant.APIKEY;
-        mFamousInfoReq.text = mEditKeyWord.getText().toString();
+        mFamousInfoReq.text = text;
         mFamousInfoReq.pattern = "ws";
         mFamousInfoReq.format = "plain";
         return mFamousInfoReq;
     }
 
     private void parseEditext() {
-        famousInfoModel.queryLookUp(initParams()).enqueue(new Callback<String>() {
+        String text = mEditKeyWord.getText().toString();
+
+        famousInfoModel.queryLookUp(initParams(text)).enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
                 String result = response.body().trim();
@@ -88,10 +97,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+
     /**
      * 选择文件解析
      */
     private void parseFile() {
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_SELECT;
+        properties.root = new File(DialogConfigs.DEFAULT_DIR);
+        properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
+        properties.offset = new File(DialogConfigs.DEFAULT_DIR);
+        properties.extensions = null;
 
+        FilePickerDialog dialog = new FilePickerDialog(MainActivity.this, properties);
+        dialog.setTitle("Select a File");
+        dialog.setDialogSelectionListener(new DialogSelectionListener() {
+            @Override
+            public void onSelectedFilePaths(String[] files) {
+                String string = FileUtils.readTxtFile(files[0]);
+                if (!TextUtils.isEmpty(string)) {
+                    Log.e(TAG, string);
+                    mEditKeyWord.setText(string);
+                    parseF(string);
+                }
+
+            }
+        });
+        dialog.show();
+    }
+
+    private void parseF(String text) {
+        famousInfoModel.queryLookUp(initParams(text)).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                String result = response.body().trim();
+                CloudResultPlainParse parse = new CloudResultPlainParse();
+                ArrayList<String> list = parse.parse(result);
+//                    Log.e(TAG, "result====" + result);
+                String string = "";
+                for (String tmp : list) {
+                    string += tmp + "\n";
+                    Log.e(TAG, tmp.toString());
+                }
+                mTxtContent.setText(string);
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mwf/analyze/utils/FileUtils.java
@@ -0,0 +1,50 @@
+package com.mwf.analyze.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by m on 2017/3/6.
+ */
+
+public class FileUtils {
+    /**
+     * 功能：Java读取txt文件的内容
+     * 步骤：1：先获得文件句柄
+     * 2：获得文件句柄当做是输入一个字节码流，需要对这个输入流进行读取
+     * 3：读取到输入流后，需要读取生成字节流
+     * 4：一行一行的输出。readline()。
+     * 备注：需要考虑的是异常情况
+     * @param filePath
+     */
+    public static String readTxtFile(String filePath){
+        try {
+//            String encoding="GBK";
+            String encoding="UTF-8";
+            File file=new File(filePath);
+            if(file.isFile() && file.exists()){ //判断文件是否存在
+                InputStreamReader read = new InputStreamReader(
+                        new FileInputStream(file),encoding);//考虑到编码格式
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = null;
+                String result="";
+                while((lineTxt = bufferedReader.readLine()) != null){
+                    System.out.println(lineTxt);
+                    result+=lineTxt;
+                }
+                read.close();
+                return  result;
+            }else{
+                System.out.println("找不到指定的文件");
+                return  null;
+            }
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+            return  null;
+        }
+
+    }
+}
