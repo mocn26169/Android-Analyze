1d0f243c00b70f9e3b900ef126a89d2d5f2d8777
mocn2619 <mocn26169@qq.com>
2017-03-06 18:11:46 +0800

    识别文件

--- app/src/main/java/com/mwf/analyze/activity/MainActivity.java
+++ app/src/main/java/com/mwf/analyze/activity/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         famousInfoModel = FamousInfoModel.getInstance(this);
+        getSupportActionBar().hide();
     }
 
     @Override
